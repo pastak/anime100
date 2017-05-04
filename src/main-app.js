2fd54980d7a7c186dfd23dcf8030d3541014859a
pastak <pasta0915@gmail.com>
2017-05-04 14:34:15 +0900

    update

--- src/main-app.js
+++ src/main-app.js
@@ -20,13 +20,16 @@ export default class extends React.Component {
     let height = 12
     for (let year = 2016; year >= 1960; year -= 3, height += 36) {
       this.lines.push(<text key={`${year}text`} x='0' y={height + 6}>{year}年</text>)
-      this.lines.push(<path key={`${year}line`} stroke='#9f9f9f' strokeWidth='1' fill='none' d={`M60,${height} L1500,${height}`}/>)
+      this.lines.push(<path key={`${year - 0}line`} stroke='#9f9f9f' strokeWidth='1' fill='none' d={`M40,${height} L1200,${height}`}/>)
+      this.lines.push(<path key={`${year - 1}line`} stroke='#d9d9d9' strokeWidth='1' fill='none' d={`M40,${height + 12} L1200,${height + 12}`}/>)
+      this.lines.push(<path key={`${year - 2}line`} stroke='#d9d9d9' strokeWidth='1' fill='none' d={`M40,${height + 24} L1200,${height + 24}`}/>)
     }
     this.lines.push(<text key='1text' x='55' y={height + 15}>1位</text>)
     this.lines.push(<path key='1line' stroke='#9f9f9f' strokeWidth='1' fill='none' d={`M67.5,0 L67.5,${height}`}/>)
     for (let rank = 10, width = 90; rank <= 409; rank += 10, width += 25) {
       this.lines.push(<text key={`${rank}text`} x={width - 9} y={height + 15} fontSize='0.8em'>{rank}</text>)
       this.lines.push(<path key={`${rank}line`} stroke='#9f9f9f' strokeWidth='1' fill='none' d={`M${width},0 L${width},${height}`}/>)
+      this.lines.push(<path key={`${rank - 5}line`} stroke='#d9d9d9' strokeWidth='1' fill='none' d={`M${width - 12.5},0 L${width - 12.5},${height}`}/>)
     }
   }
 
@@ -63,7 +66,7 @@ export default class extends React.Component {
       <button onClick={() => this.setRankingType('male')} className={`btn ${this.state.rankingType === 'male' ? 'btn-success' : 'btn-default'}`}>男性トップ100</button>
       <button onClick={() => this.setRankingType('female')} className={`btn ${this.state.rankingType === 'female' ? 'btn-success' : 'btn-default'}`}>女性トップ100</button>
       <br /><br />
-      <svg id='svg' width='1200' height='850'>
+      <svg id='svg' width='1080' height='850'>
         {this.lines}
         <DataDots
           showPinnedInfo={this.showPinnedInfo}
